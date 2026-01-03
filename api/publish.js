d12837d243c07393bdb5c0ee4b7c576fcda1c706
anthonynadjari <nadjari.anthony@gmail.com>
2026-01-03 15:01:35 +0100

    Fix Vercel function - use CommonJS (module.exports)

--- api/publish.js
+++ api/publish.js
@@ -11,7 +11,7 @@
  * - GITHUB_REPO: Repository in format "username/repo"
  */
 
-export default async function handler(req, res) {
+module.exports = async function handler(req, res) {
   // Enable CORS
   res.setHeader('Access-Control-Allow-Origin', '*');
   res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
